8fd82fc7c2d5c7d8eb512d8985b93216265d8f7e
lucho19jose <barbozagonzalesjose@gmail.com>
2023-01-17 17:22:21 -0500

    async querys

--- services/product.service.js
+++ services/product.service.js
@@ -7,7 +7,6 @@ class ProductService {
   }
 
   generate() {
-
     const limit = 100;
     for(let index = 0; index < limit; index++){
       this.products.push({
@@ -19,7 +18,7 @@ class ProductService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -28,15 +27,15 @@ class ProductService {
     return newProduct;
   }
 
-  find() {
+  async find() {
     return this.products;
   }
 
-  findOne(id){
+  async findOne(id){
     return this.products.find(item => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
       throw new Error('Product not found');
@@ -49,7 +48,7 @@ class ProductService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
       throw new Error('Product not found');
