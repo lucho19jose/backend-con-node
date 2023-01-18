4e5e8e23b3bbd31ad68fc29440c4b22e721d2e6f
lucho19jose <barbozagonzalesjose@gmail.com>
2023-01-18 11:26:56 -0500

    cors

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middelwares/error.handler');
-const { use } = require('./routes/products.router');
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.pe']
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    }else{
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 routerApi(app);
 
