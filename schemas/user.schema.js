d05e5b0db1c1ccf4802f8657bbfe91eb79f9f4be
lucho19jose <barbozagonzalesjose@gmail.com>
2023-01-23 11:48:59 -0500

    modifying an entity

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -4,20 +4,20 @@ const userId = Joi.string();
 //const user = Joi.string().min(3);
 const email = Joi.string().email({ tlds: { allow: false } });
 const password = Joi.string().min(4);
-// const role = Joi.string().min(5);
+const role = Joi.string().min(5);
 
 const createUserSchema = Joi.object({
   //user: user.required(),
   email: email.required(),
   password: password.required(),
-  //role: role.required()
+  role: role.required()
 });
 
 const updateUserSchema = Joi.object({
   //user: user,
   email: email,
   //password: password,
-  //role: role,
+  role: role,
 });
 
 const getUserSchema = Joi.object({
